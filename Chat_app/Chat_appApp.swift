93051118ccffcd6d8cd1fb6cce964dccfc235f7e
john3c <john3corrigan@gmail.com>
2023-01-25 19:26:36 -0500

    Initial Commit

---
+++ Chat_app/Chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_appApp.swift
+//  Chat_app
+//
+//  Created by Corrigan, John on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
