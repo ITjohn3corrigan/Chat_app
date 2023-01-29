c94aa3cb4185d67111e19bdf181ea22c280f216a
ITjohn3corrigan <john3corrigan@gmail.com>
2023-01-29 14:04:52 -0500

    change to fonts

---
+++ Chat_app/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Chat_app
+//
+//  Created by Corrigan, John on 1/29/23.
+//
+
+import Foundation
+import SwiftUI
+import Firebase
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
